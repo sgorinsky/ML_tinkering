8f7327517a1d700e929fb20da1fb5318b53b15ae
Sam Gorinsky <sgorinsky@gmail.com>
2018-08-04 21:42:16 -0400

    included graph of predicted stock

--- linear_reg_GOOG.py
+++ linear_reg_GOOG.py
@@ -8,14 +8,18 @@ Created on Thu Aug  2 22:10:12 2018
 
 import pandas as pd
 import numpy as np
-import quandl, math
+import quandl, math, datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import pickle
 
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL', api_key = 'sdHHWLieXUJfop1HqnLX')
 
-print(df.head())
+#print(df.head())
 #print(df.tail())
 
 df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
@@ -24,25 +28,51 @@ df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
-print(df.head())
 
 forecast_col = 'Adj. Close'
-df.fillna(value=-99999, inplace=True)
 forecast_out = int(math.ceil(0.01 * len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
+
 
 X = np.array(df.drop(['label'], 1))
 X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+df.dropna(inplace=True)
+
 y = np.array(df['label'])
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
-#clf = svm.SVR(kernel = 'poly')
-clf = LinearRegression(n_jobs = 5)
-clf.fit(X_train, y_train)
+#clf = LinearRegression(n_jobs=-1)
+#clf.fit(X_train, y_train)
+#with open('linearregression.picle','wb') as f:
+#    pickle.dump(clf, f)
+    
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)    
+
 accuracy = clf.score(X_test, y_test)
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
 
-print(accuracy)
+df['Forecast'] = np.nan
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
 
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+    
+print(df['Forecast'].tail())  
+  
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
